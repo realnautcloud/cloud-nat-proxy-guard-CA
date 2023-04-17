049f4004a90fc4ceb501d180111374b7621e1f69
Your Name <you@example.com>
2023-04-17 23:45:52 +0200

    Use vercel/ncc

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require = require('esm')(module /*, options*/);
 
-const execa = require('execa');
-
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -11,27 +9,13 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 
-//async function runCommand(command) {
-//  const { stdout, stderr } = await exec(command, {stdio: "inherit"});
-//  console.log('stdout:', stdout);
-//  console.error('stderr:', stderr);
-//}
-
-
 async function runCommand(command) {
-  try {
-    const { stdout, stderr, exitCode } = await execa.command(command);
-    console.log('stdout:', stdout);
-    console.error('stderr:', stderr);
-    if (exitCode === 0) {
-      console.log('WireGuard se ha iniciado correctamente');
-    } else {
-      console.error(`WireGuard no se ha iniciado correctamente. Código de salida: ${exitCode}`);
-    }
-  } catch (error) {
-    console.error(`Error al ejecutar el comando: ${error}`);
-  }
+  const { stdout, stderr } = await exec(command, {stdio: "inherit"});
+  console.log('stdout:', stdout);
+  console.error('stderr:', stderr);
 }
+
+
 
 async function run() {
     try {
@@ -41,8 +25,9 @@ async function run() {
       if (taskParam === 'start') {
         
         console.log('Descargando e instalando WireGuard...');
-        await runCommand('sudo apt-get update && sudo apt-get install -y wireguard');
-        console.log('WireGuard instalado.');
+        await exec('sudo add-apt-repository ppa:wireguard/wireguard -y');
+        await exec('sudo apt-get update && sudo apt-get install -y wireguard');
+        console.log('WireGuard instalado.')
 
         console.log('Decodificando y escribiendo configuración...');
         const configStr = Buffer.from(peerParam, 'base64').toString();
@@ -53,7 +38,7 @@ async function run() {
 
         try {
           console.log(`sudo wg-quick up ${configPath}`);
-          await runCommand(`sudo wg-quick up ${configPath}`);
+          await exec(`sudo wg-quick up ${configPath}`);
           console.log('WireGuard iniciado correctamente');
         } 
         catch (error) {
